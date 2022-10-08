9cde807d1d11d56240a15f5838a8abbab3323b48
Ryan Mehta <ryan.mehta@gmail.com>
2022-10-07 22:51:47 -0700

    add prisma

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import DashboardLayout from "../components/layouts/DashboardLayout";
+
+const Home: NextPage = () => {
+  return (
+    <DashboardLayout title="Dashboard">
+      <h1>hi</h1>
+    </DashboardLayout>
+  );
+};
+
+export default Home;
