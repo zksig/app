edd3b4b0f952c5dc2d84e0d23d9e08d209e78411
Lance Merill <lancemerill@Lances-MacBook-Pro-2.local>
2022-10-20 13:36:14 -0700

    x

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,54 @@
-import "./styles/globals.css";
-import type { AppProps } from "next/app";
+
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { UnsafeBurnerWalletAdapter, PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { clusterApiUrl } from '@solana/web3.js';
+import type { AppProps } from 'next/app';
+import type { FC } from 'react';
 import { UserProvider } from "@auth0/nextjs-auth0";
+import React, { useMemo } from 'react';
+import "./styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <UserProvider>
-      <Component {...pageProps} />
-    </UserProvider>
-  );
-}
+// Use require instead of import since order matters
+require('@solana/wallet-adapter-react-ui/styles.css');
+// require('../styles/globals.css');
 
-export default MyApp;
+const App: FC<AppProps> = ({ Component, pageProps }) => {
+    // Can be set to 'devnet', 'testnet', or 'mainnet-beta'
+    const network = WalletAdapterNetwork.Devnet;
+
+    // You can also provide a custom RPC endpoint
+    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+    const wallets = useMemo(
+        () => [
+            /**
+             * Select the wallets you wish to support, by instantiating wallet adapters here.
+             *
+             * Common adapters can be found in the npm package `@solana/wallet-adapter-wallets`.
+             * That package supports tree shaking and lazy loading -- only the wallets you import
+             * will be compiled into your application, and only the dependencies of wallets that
+             * your users connect to will be loaded.
+             */
+            new UnsafeBurnerWalletAdapter(),
+            new PhantomWalletAdapter()
+        ],
+        []
+    );
+
+    return (
+      <UserProvider>
+        <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={wallets} autoConnect>
+                <WalletModalProvider>
+                    <Component {...pageProps} />
+                </WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
+      </UserProvider>
+    );
+};
+
+export default App;
+
