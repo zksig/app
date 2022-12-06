fa1fa5c3be12bdd19e7c00085df7b4afd7ff93f1
Adrian Monasterios Gutierrez <adri.suscripciones@gmail.com>
2022-12-06 17:18:21 -0500

    svg properties fix

--- src/components/agreements/CreateAgreement.tsx
+++ src/components/agreements/CreateAgreement.tsx
@@ -39,13 +39,13 @@ const ConfigureAgreement = ({
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              stroke-width="1.5"
+              strokeWidth="1.5"
               stroke="currentColor"
               className="mx-auto h-12 w-12 text-gray-400 text-green-500"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 d="M4.5 12.75l6 6 9-13.5"
               />
             </svg>
@@ -533,9 +533,9 @@ const CreateAgreement = () => {
                       viewBox="0 0 24 24"
                       fill="none"
                       stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                       className="mr-3 h-5 w-5 animate-spin-slow"
                     >
                       <line x1="12" y1="2" x2="12" y2="6"></line>
