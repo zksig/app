2e7a759e955a06df4e803d1e3371eb3ba24bd2f4
Adrian Monasterios Gutierrez <adri.suscripciones@gmail.com>
2022-12-06 17:21:47 -0500

    create new page

---
+++ src/pages/agreements/create-new.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import NewCreateAgreement from "../../components/agreements/NewCreateAgreement";
+
+const CreateAgreementPageNew: NextPage = () => {
+  return <NewCreateAgreement />;
+};
+
+export default CreateAgreementPageNew;
